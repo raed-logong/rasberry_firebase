2f48d22dbe807dd3159a381da64b8459b7c55b64
Raed Attia <logong3@hotmail.fr>
2021-12-07 22:28:21 +0100

    first

---
+++ main.py
@@ -0,0 +1,55 @@
+# import RPi.GPIO as GPIO
+# GPIO.setwarnings(False)
+# GPIO.setmode(GPIO.BOARD)
+# GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+from datetime import datetime
+# from picamera import PiCamera
+from time import sleep
+import os
+
+import pyrebase
+
+firebaseConfig = {
+    "apiKey": "AIzaSyC-BUMN0WB14YUuebX-97Jv_oNCVn3__D4",
+    "authDomain": "rpi-images-a1244.firebaseapp.com",
+    "databaseURL": "https://rpi-images-a1244-default-rtdb.europe-west1.firebasedatabase.app",
+    "projectId": "rpi-images-a1244",
+    "storageBucket": "rpi-images-a1244.appspot.com",
+    "messagingSenderId": "994685609465",
+    "appId": "1:994685609465:web:9250687bff32c971780928",
+    "measurementId": "${config.measurementId}"
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+database = firebase.database()
+storage = firebase.storage()
+
+# camera = PiCamera()
+
+# while True:
+#     try:
+#         if GPIO.input(10) == GPIO.HIGH:
+#             print("pushed")
+#             now = datetime.now()
+#             dt = now.strftime("%d%m%Y%H:%M:%S")
+#             name = dt+".jpg"
+#             camera.capture(name)
+#             print(name+" saved")
+#             storage.child(name).put(name)
+#             print("Image sent")
+#             os.remove(name)
+#             print("File Removed")
+#             sleep(2)
+#
+#
+#     except:
+#         camera.close()
+if __name__ == '__main__':
+    now = datetime.now()
+    dt = now.strftime("%d%m%Y%H:%M:%S")
+    name = "test.jpg"
+
+    storage.child(dt + '.jpg').put(name)
+    url = storage.child(dt + '.jpg').get_url('')
+    print(url)
+    database.child('images').child(dt).set({'path': url})
